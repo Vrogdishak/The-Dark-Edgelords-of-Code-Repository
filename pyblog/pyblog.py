812eaa0e20d892036294bfe9de1d4628dab55c38
leblanck <kyle@leblanc.sh>
2022-12-02 14:47:19 -0500

    used/tested env variable for requests

--- pyblog/pyblog.py
+++ pyblog/pyblog.py
@@ -8,6 +8,7 @@ import requests
 # WORDPRESS_USERNAME = os.environ["WORDPRESS_USERNAME"]
 # WORDPRESS_PASSWORD = os.environ["WORDPRESS_PASSWORD"]
 # WORDPRESS_URL = os.environ["WORDPRESS_URL"]
+WORDPRESS_URL = "http://ec2-44-202-229-125.compute-1.amazonaws.com:8088/wp-json"
 
 # MENU
 # - List All Post Titles
@@ -16,17 +17,16 @@ import requests
 # - Exit
 
 def getPostTitles():
-    response = requests.get("http://ec2-44-202-229-125.compute-1.amazonaws.com:8088/wp-json/wp/v2/posts")
+    response = requests.get(WORDPRESS_URL+"/wp/v2/posts")
     data = response.json()
     
     for i in range(len(data)):
         title = data[i]["title"]["rendered"]
         print(str(i) + ": " + title)
 
-def getSinglePost():
-    pass
-
-# response = requests.get("http://ec2-44-202-229-125.compute-1.amazonaws.com:8088/wp-json/wp/v2/posts/1")
+def getSinglePost(postID):
+    response = requests.get(WORDPRESS_URL+"/wp/v2/posts/"+postID)
+    data = response.json()
 
 # data = response.json()
 
